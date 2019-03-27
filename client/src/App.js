1cf38f3e4c4f74210b16bdaedf803720523c1e7f
Your Name <jawadalikhel18@gmail.com>
2019-03-27 09:00:54 -0500

    add the redux store and reducers folders

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavlbar.js';
 import ShoppingList from './components/ShoppingList.js';
 
+import {Provider} from 'react-redux';
+import store from './store';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <ShoppingList />
+        </div>
+      </Provider>
     );
   }
 }
---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,9 @@
+import uuid from 'uuid';
+const initialState = {
+  items: [
+    {id: uuid(),name: 'Eggs'},
+    {id: uuid(),name: 'Bean'},
+    {id: uuid(),name: 'Potatos'},
+    {id: uuid(),name: 'Turkey'},
+  ]
+}
